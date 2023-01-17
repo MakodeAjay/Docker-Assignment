4651b4c372747438de0bbf051f182841f055d0eb
Ajay Makode <ajaymakode402@gmail.com>
2023-01-17 16:45:47 +0530

    Added all files

---
+++ Infracubator/DockerAssignment/Assignment2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Input values"
+read a b
+echo "Addition : $(( a + b ))"
+echo "Substraction : $((a - b ))" 
+echo "Multiplication : $(( a * b ))"  
+echo "Division : $(( a / b ))"
